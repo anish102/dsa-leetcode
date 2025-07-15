f539c7c44315927f2e89cfa62e342688c688b53d
Anish Karki <anishkarki695@gmail.com>
2025-07-15 22:54:20 +0545

    Optimized solution for maximum profit of stock

--- best_time_to_buy_and_sell_stock/stock.py
+++ best_time_to_buy_and_sell_stock/stock.py
@@ -7,6 +7,7 @@ Return the maximum profit you can achieve. You may choose to not make any transa
 
 # Brute force solution with O(n^2) time complexity.
 # We iterate through each pair of days and calculate the profit.
+# Time complexity: O(n^2)
 def maxProfit(prices: list[int]) -> int:
     max_profit = 0
     count = range(len(prices))
@@ -17,5 +18,20 @@ def maxProfit(prices: list[int]) -> int:
                 max_profit = profit if profit > max_profit else max_profit
     return max_profit
 
+
+# Optimized solution with O(n) time complexity.
+# We keep track of the minimum price seen so far and calculate the profit at each step.
+# Time complexity: O(n)
+def maxProfitOptimized(prices: list[int]) -> int:
+    max_profit = 0
+    min_price = max(prices)
+    for price in prices:
+        min_price = min(price, min_price)
+        cur_profit = price - min_price
+        max_profit = max(cur_profit, max_profit)
+    return max_profit
+
+
 prices = [7, 1, 5, 3, 6, 4]
 print(maxProfit(prices))
+print(maxProfitOptimized(prices))
